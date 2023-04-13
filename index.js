288899218593b6190db9515e635dbc20eee96247
EuryJesus <Eury0218@hotmail.com>
2023-04-13 12:58:02 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function  setBestCustomer() {
+
+return 'maybe bob'
+}
+var bestCustomer = 'maybe bob'
+function overwriteBestCustomer() {
+    setBestCustomer();
+return 'maybe bob'
+}
+function changeLeastFavoriteCustomer() {
+ const leastFavoriteCustomer = '22' 
+    console.log(leastFavoriteCustomer);
+    leastFavoriteCustomer = '21'
+ 
+ 
+}
